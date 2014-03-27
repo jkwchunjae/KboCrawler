a6efef8748bdf399ebe887cecbcfaf4663013b7b
jkwchunjae <jkwchunjae@daum.net>
2014-03-27 13:40:41 +0900

    실패했을 때만 로그 남기도록 수정

--- KBO-Crawling/KBO-Crawling/Standing.cs
+++ KBO-Crawling/KBO-Crawling/Standing.cs
@@ -6,116 +6,116 @@ using System.Threading.Tasks;
 
 namespace KBO_Crawling
 {
-   class Standing
-   {
-      public DateTime Date;
-      public int Rank;
-      public string TeamName;
-      public int TeamIndex;
-      public int Game;
-      public int Win;
-      public int Lose;
-      public int Draw;
-      public double PCT;
-      public double GB;
-      public string STRK;
-      public string L10;
+    class Standing
+    {
+        public DateTime Date;
+        public int Rank;
+        public string TeamName;
+        public int TeamIndex;
+        public int Game;
+        public int Win;
+        public int Lose;
+        public int Draw;
+        public double PCT;
+        public double GB;
+        public string STRK;
+        public string L10;
 
-      public bool WriteInfo(string[] teamArr)
-      {
-         int tmp;
-         double dbl;
+        public bool WriteInfo(string[] teamArr)
+        {
+            int tmp;
+            double dbl;
 
-         #region 순위
-         if (int.TryParse(teamArr[0], out tmp))
-         {
-            Rank = tmp;
-         }
-         else
-         {
-            return false;
-         }
-         #endregion
+            #region 순위
+            if (int.TryParse(teamArr[0], out tmp))
+            {
+                Rank = tmp;
+            }
+            else
+            {
+                return false;
+            }
+            #endregion
 
-         #region 구단 이름
-         TeamName = teamArr[1];
-         #endregion
+            #region 구단 이름
+            TeamName = teamArr[1];
+            #endregion
 
-         #region 경기 수
-         if (int.TryParse(teamArr[2], out tmp))
-         {
-            Game = tmp;
-         }
-         else
-         {
-            return false;
-         }
-         #endregion
+            #region 경기 수
+            if (int.TryParse(teamArr[2], out tmp))
+            {
+                Game = tmp;
+            }
+            else
+            {
+                return false;
+            }
+            #endregion
 
-         #region 승
-         if (int.TryParse(teamArr[3], out tmp))
-         {
-            Win = tmp;
-         }
-         else
-         {
-            return false;
-         }
-         #endregion
+            #region 승
+            if (int.TryParse(teamArr[3], out tmp))
+            {
+                Win = tmp;
+            }
+            else
+            {
+                return false;
+            }
+            #endregion
 
-         #region 패
-         if (int.TryParse(teamArr[4], out tmp))
-         {
-            Lose = tmp;
-         }
-         else
-         {
-            return false;
-         }
-         #endregion
+            #region 패
+            if (int.TryParse(teamArr[4], out tmp))
+            {
+                Lose = tmp;
+            }
+            else
+            {
+                return false;
+            }
+            #endregion
 
-         #region 무
-         if (int.TryParse(teamArr[5], out tmp))
-         {
-            Draw = tmp;
-         }
-         else
-         {
-            return false;
-         }
-         #endregion
+            #region 무
+            if (int.TryParse(teamArr[5], out tmp))
+            {
+                Draw = tmp;
+            }
+            else
+            {
+                return false;
+            }
+            #endregion
 
-         #region 승률
-         if (double.TryParse(teamArr[6], out dbl))
-         {
-            PCT = dbl;
-         }
-         else
-         {
-            return false;
-         }
-         #endregion
+            #region 승률
+            if (double.TryParse(teamArr[6], out dbl))
+            {
+                PCT = dbl;
+            }
+            else
+            {
+                return false;
+            }
+            #endregion
 
-         #region 승차
-         if (double.TryParse(teamArr[7], out dbl))
-         {
-            GB = dbl;
-         }
-         else
-         {
-            return false;
-         }
-         #endregion
+            #region 승차
+            if (double.TryParse(teamArr[7], out dbl))
+            {
+                GB = dbl;
+            }
+            else
+            {
+                return false;
+            }
+            #endregion
 
-         #region 연속
-         STRK = teamArr[8];
-         #endregion
+            #region 연속
+            STRK = teamArr[8];
+            #endregion
 
-         #region 최근10경기
-         L10 = "";
-         #endregion
+            #region 최근10경기
+            L10 = "";
+            #endregion
 
-         return true;
-      }
-   }
+            return true;
+        }
+    }
 }
--- KBO-Crawling/KBO-Crawling/Utils.cs
+++ KBO-Crawling/KBO-Crawling/Utils.cs
@@ -6,88 +6,88 @@ using System.Threading.Tasks;
 
 namespace KBO_Crawling
 {
-   public static class Utils
-   {
-      private static Dictionary<string, string> engNameDic = new Dictionary<string, string>();
+    public static class Utils
+    {
+        private static Dictionary<string, string> engNameDic = new Dictionary<string, string>();
 
-      static Utils()
-      {
-         engNameDic.Add("삼성", "SS");
-         engNameDic.Add("넥센", "NX");
-         engNameDic.Add("LG", "LG");
-         engNameDic.Add("롯데", "LT");
-         engNameDic.Add("KIA", "KA");
-         engNameDic.Add("두산", "DS");
-         engNameDic.Add("SK", "SK");
-         engNameDic.Add("NC", "NC");
-         engNameDic.Add("한화", "HH");
-         engNameDic.Add("현대", "NX");
-      }
+        static Utils()
+        {
+            engNameDic.Add("삼성", "SS");
+            engNameDic.Add("넥센", "NX");
+            engNameDic.Add("LG", "LG");
+            engNameDic.Add("롯데", "LT");
+            engNameDic.Add("KIA", "KA");
+            engNameDic.Add("두산", "DS");
+            engNameDic.Add("SK", "SK");
+            engNameDic.Add("NC", "NC");
+            engNameDic.Add("한화", "HH");
+            engNameDic.Add("현대", "NX");
+        }
 
-      public static DateTime ToDateTime(this int date)
-      {
-         return new DateTime(date / 10000, (date / 100) % 100, date % 100);
-      }
+        public static DateTime ToDateTime(this int date)
+        {
+            return new DateTime(date / 10000, (date / 100) % 100, date % 100);
+        }
 
-      public static int ToInt(this DateTime date)
-      {
-         return date.Year * 10000 + date.Month * 100 + date.Day;
-      }
+        public static int ToInt(this DateTime date)
+        {
+            return date.Year * 10000 + date.Month * 100 + date.Day;
+        }
 
-      public static DateTime GetDate(int input)
-      {
-         /*
-          * 사용자 인풋에 따라 값을 다르게 처리.
-          * 0 : 오늘
-          * 음수 : 오늘 - 인풋 (상대참조)
-          * 1 ~ 999 9999 : 잘못 입력된 날짜
-          * 그 이상 양수 : 날짜로 변환 (절대참조)
-          */
-         var date = DateTime.Now;
-         if (input == 0)
-         {
-            date = DateTime.Now;
-         }
-         else if (input < 0)
-         {
-            date = DateTime.Now.AddDays(input);
-         }
-         else if (input < 9999999)
-         {
-            LogHelper.Log(input + " : Fail");
-            date = new DateTime(1989, 2, 1);
-         }
-         else
-         {
-            try
+        public static DateTime GetDate(int input)
+        {
+            /*
+             * 사용자 인풋에 따라 값을 다르게 처리.
+             * 0 : 오늘
+             * 음수 : 오늘 - 인풋 (상대참조)
+             * 1 ~ 999 9999 : 잘못 입력된 날짜
+             * 그 이상 양수 : 날짜로 변환 (절대참조)
+             */
+            var date = DateTime.Now;
+            if (input == 0)
             {
-               date = input.ToDateTime();
+                date = DateTime.Now;
             }
-            catch (Exception ex)
+            else if (input < 0)
             {
-               LogHelper.Log(ex);
-               date = new DateTime(1989, 2, 1);
+                date = DateTime.Now.AddDays(input);
             }
-         }
-         return date;
-      }
+            else if (input < 9999999)
+            {
+                LogHelper.Log(input + " : Fail");
+                date = new DateTime(1989, 2, 1);
+            }
+            else
+            {
+                try
+                {
+                    date = input.ToDateTime();
+                }
+                catch (Exception ex)
+                {
+                    LogHelper.Log(ex);
+                    date = new DateTime(1989, 2, 1);
+                }
+            }
+            return date;
+        }
 
-      public static void Dump(this object obj)
-      {
-         Console.WriteLine(obj.ToString());
-      }
+        public static void Dump(this object obj)
+        {
+            Console.WriteLine(obj.ToString());
+        }
 
-      public static string GetShortEngName(this string teamName)
-      {
-         if (engNameDic.ContainsKey(teamName))
-         {
-            return engNameDic[teamName];
-         }
-         else
-         {
-            LogHelper.Log("Unknown TeamName : {0}", teamName);
-            return "??";
-         }
-      }
-   }
+        public static string GetShortEngName(this string teamName)
+        {
+            if (engNameDic.ContainsKey(teamName))
+            {
+                return engNameDic[teamName];
+            }
+            else
+            {
+                LogHelper.Log("Unknown TeamName : {0}", teamName);
+                return "??";
+            }
+        }
+    }
 }
